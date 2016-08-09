1825093ad45e2ec24f972860ede9daa32a7da4da
ghalibse <ghalibse@gmail.com>
2016-08-09 16:38:10 -0400

    First Commit from Libu

--- app/src/main/java/com/example/simplemap/MainActivity.java
+++ app/src/main/java/com/example/simplemap/MainActivity.java
@@ -4,25 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MainActivityTAG_";
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.a_main_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.a_main_maps);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "onMapReady: ");
+
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng tanzania = new LatLng(-6.369028, 34.8888);
+        mMap.addMarker(new MarkerOptions().position(tanzania).title("Marker in Tanzania"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tanzania));
     }
 }
